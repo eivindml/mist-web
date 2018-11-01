c9f5d07eb2e6924a1d8b8a10a36aeae660b2bf71
Eivind Mikael Lindbråten <eivindml@icloud.com>
2018-11-01 19:34:46 +0100

    Lint file

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-var dotenv = require('dotenv').config({});
-var dotenvExpand = require('dotenv-expand')(dotenv);
-var requireDir = require('require-dir')('./gulp_tasks');
-var gulp = require('gulp');
+var dotenv = require('dotenv').config({})
+var dotenvExpand = require('dotenv-expand')(dotenv)
+var requireDir = require('require-dir')('./gulp_tasks')
+var gulp = require('gulp')
 
-gulp.task('build', ['images', 'sass', 'scripts']);
-gulp.task('default', ['build']);
+gulp.task('build', ['images', 'sass', 'scripts'])
+gulp.task('default', ['build'])
