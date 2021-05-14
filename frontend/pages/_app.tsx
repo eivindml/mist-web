416806f1cac5e4374b3ef2c1742d8846f807c125
Eivind Lindbråten <eivindml@icloud.com>
2021-05-14 19:28:43 +0000

    Add tracking

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,9 +1,36 @@
 import "../reset.css";
 import "../styles.css";
 import Head from "next/head";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as Fathom from "fathom-client";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    // Example: yourdomain.com
+    //  - Do not include https://
+    //  - This must be an exact match of your domain.
+    //  - If you're using www. for your domain, make sure you include that here.
+    Fathom.load("RXUYTZOL", {
+      includedDomains: ["madebymist.com"],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, []);
+
   return (
     <>
       <Head>
